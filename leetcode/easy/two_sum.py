eb8cac3fdca0fff65c1824c2aae147151bc6a049
Guillermo <guilleelias25@gmail.com>
2026-05-02 16:31:36 -0500

    starting with problem

---
+++ leetcode/easy/two_sum.py
@@ -0,0 +1,15 @@
+Given an array of integers nums and an integer target,
+return the indices of the two numbers that add up to target.
+
+Example:
+  Input:  nums = [2, 7, 11, 15], target = 9
+  Output: [0, 1]  (because nums[0] + nums[1] = 2 + 7 = 9)   
+
+def two_sum(nums, target):
+    # your code here
+    pass
+
+# Test it
+print(two_sum([2, 7, 11, 15], 9))   # Expected: [0, 1]
+print(two_sum([3, 2, 4], 6))         # Expected: [1, 2]
+print(two_sum([3, 3], 6))            # Expected: [0, 1]
