dd84c7a1bb9c619c506a6c32e1819765af254c31
Guillermo Barreto <124272218+GuillermoBarreto@users.noreply.github.com>
2026-09-20 20:47:33 -0500

    Fix two_sum.py: comment out bare Example block that caused SyntaxError

--- leetcode/easy/two_sum.py
+++ leetcode/easy/two_sum.py
@@ -1,9 +1,9 @@
 # Given an array of integers nums and an integer target,
 # return the indices of the two numbers that add up to target.
 
-Example:
-  Input:  nums = [2, 7, 11, 15], target = 9
-  Output: [0, 1]  (because nums[0] + nums[1] = 2 + 7 = 9)   
+# Example:
+# Input:  nums = [2, 7, 11, 15], target = 9
+# Output: [0, 1]  (because nums[0] + nums[1] = 2 + 7 = 9)
 
 class Solution:
     def twoSum(self, nums, target):
